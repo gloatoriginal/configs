05256abfba25a60fc52e9078c3b22a3ffaa33dd1
Devin Baughman <devbau1993@gmail.com>
2023-08-17 21:14:09 -0700

    init

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- Enable line numbers and relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Mouse settings
+vim.opt.mouse = 'a'
+vim.opt.wrap = true
+vim.opt.scroll = 3
+
